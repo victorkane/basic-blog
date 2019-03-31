cfdbd8d0d92e34d6c7bcc3dd7c17a7b153b51be4
Victor Kane <victorkane@gmail.com>
2019-03-31 05:45:23 -0400

    hooks based solution

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,76 +1,67 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
-//import InputGroup from 'react-bootstrap/InputGroup'
-//import FormControl from 'react-bootstrap/FormControl'
+import './index.scss'
+
 // file obtained by: curl https://jsonplaceholder.typicode.com/posts > posts.json
 import posts from '../../data/posts.json'
-import './index.scss'
 
 const categories = [ 'catagory1', 'category2', 'category3', 'category4' ]
 
-class Posts extends Component {
-  // For random number see https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-  state = {
-    posts: posts.slice(0, 8).map(post => { return { ...post, category: categories[Math.floor(Math.random() * 4)] }}),
-    categories: categories,
-		selectedCategory: ""
-  }
+// For random number see https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+const somePosts = posts.slice(0, 8).map(post => { return { ...post, category: categories[Math.floor(Math.random() * 4)] }})
 
-  handleOptionChange = changeEvent => {
-	  this.setState({
-      selectedCategory: changeEvent.target.value
-    })
+const Posts = () => {
+  const [postList] = useState(somePosts)
+  const [categoryList] = useState(categories)
+  const [selectedCategory, setCategory] = useState("")
+  const handleOptionChange = changeEvent => {
+	  setCategory(changeEvent.target.value)
   }
-
-	handleFormSubmit = formSubmitEvent => {
+  const handleFormSubmit = formSubmitEvent => {
 	  formSubmitEvent.preventDefault();
-		this.setState({
-		  selectedCategory: ""
-		})
+		setCategory("")
 	};
-
-  render () {
-    const thePosts = this.state.posts.map(post => {
-		  if (this.state.selectedCategory === "" || this.state.selectedCategory === post.category) {
-        return <ListGroup.Item
-          key={post.id}
-          variant="flush">
-          <h5>{post.title}</h5>
-          {post.body.substr(0,80) + '...'}
-					<small><strong>{post.category}</strong></small>
-        </ListGroup.Item>
-		  } else {
-			  return null;
-			}
-    })
-    const theCategoryOptions = this.state.categories.map((category,i) => {
-		  return <div className="form-check" key={i}>
-			  <label>
+  const thePosts = postList.map(post => {
+	  if (selectedCategory === "" || selectedCategory === post.category) {
+      return <ListGroup.Item
+        key={post.id}
+        variant="flush">
+        <h5>{post.title}</h5>
+        {post.body.substr(0,80) + '...'}
+				<small><strong>{post.category}</strong></small>
+      </ListGroup.Item>
+		} else {
+		  return null;
+	  }
+  })
+  const theCategoryOptions = categoryList.map((category,i) => {
+		return <div className="form-check" key={i}>
+        <label>
 				  <input
-                  type="radio"
-                  name="react-tips"
-                  value={category}
-                  checked={this.state.selectedCategory === category }
-									onChange={this.handleOptionChange}
-                  className="form-check-input"
-					/>
-					{category}
+            type="radio"
+            name="react-tips"
+            value={category}
+            checked={selectedCategory === category }
+					  onChange={handleOptionChange}
+            className="form-check-input"
+			    />
+					  {category}
 			  </label>
-			</div>
+      </div>
     })
-    return (
-			<>
-        <Container className="Filter mb-4">
+  return (
+  	<>
+       <Container className="Filter mb-4">
           <Row>
 				    <Col className="mt-4 p-4" sm={12}>
 				      <Card>
 				        <Card.Title className="px-4 pt-4 pb-0">Filter by Post category</Card.Title>
 				        <Card.Body>
-                    <form onSubmit={this.handleFormSubmit}>
+                    <form onSubmit={handleFormSubmit}>
 					            {theCategoryOptions}
                       <div className="form-group">
 				                <div>Show posts with selected category</div>
@@ -96,9 +87,8 @@ class Posts extends Component {
             </Col>
           </Row>
         </Container>
-		  </>
-    )
-  }
+		</>
+  )
 }
 
 export default Posts
