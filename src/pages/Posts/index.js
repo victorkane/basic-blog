a464d0b5df61d9954638f787a6731f4eb86c4a8e
Victor Kane <victorkane@gmail.com>
2019-03-31 04:03:23 -0400

    Implemented filter by category selected in option list

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -15,7 +15,7 @@ const categories = [ 'catagory1', 'category2', 'category3', 'category4' ]
 class Posts extends Component {
   // For random number see https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
   state = {
-    posts: posts.slice(0, 6).map(post => { return { ...post, category: categories[Math.floor(Math.random() * 4)] }}),
+    posts: posts.slice(0, 8).map(post => { return { ...post, category: categories[Math.floor(Math.random() * 4)] }}),
     categories: categories,
 		selectedCategory: ""
   }
@@ -28,21 +28,27 @@ class Posts extends Component {
 
 	handleFormSubmit = formSubmitEvent => {
 	  formSubmitEvent.preventDefault();
-		  console.log("You have submitted:", this.state.selectedOption);
+		this.setState({
+		  selectedCategory: ""
+		})
 	};
 
   render () {
     const thePosts = this.state.posts.map(post => {
-      return <ListGroup.Item
+		  if (this.state.selectedCategory === "" || this.state.selectedCategory === post.category) {
+        return <ListGroup.Item
           key={post.id}
           variant="flush">
           <h5>{post.title}</h5>
           {post.body.substr(0,80) + '...'}
 					<small><strong>{post.category}</strong></small>
         </ListGroup.Item>
+		  } else {
+			  return null;
+			}
     })
-    const theCategoryOptions = this.state.categories.map(category => {
-		  return <div className="form-check">
+    const theCategoryOptions = this.state.categories.map((category,i) => {
+		  return <div className="form-check" key={i}>
 			  <label>
 				  <input
                   type="radio"
@@ -51,7 +57,6 @@ class Posts extends Component {
                   checked={this.state.selectedCategory === category }
 									onChange={this.handleOptionChange}
                   className="form-check-input"
-									inline
 					/>
 					{category}
 			  </label>
@@ -59,40 +64,38 @@ class Posts extends Component {
     })
     return (
 			<>
-        <Container className="Filter">
+        <Container className="Filter mb-4">
           <Row>
 				    <Col className="mt-4 p-4" sm={12}>
-				     <Card>
-				       <Card.Title className="px-4 pt-4 pb-0">Filter by Post category</Card.Title>
-				       <Card.Body>
-				         <Card.Text>
-                  <form onSubmit={this.handleFormSubmit}>
-					          {theCategoryOptions}
-                    <div className="form-group">
-				              <p>Show posts with selected category</p>
-                      <button className="btn btn-primary mt-2" type="submit">
-                        Select
-                      </button>
-                    </div>
-                  </form>
-				         </Card.Text>
-				       </Card.Body>
-				     </Card>
-          </Col>
-				</Row>
-			</Container>
-      <Container className="Posts">
-        <Row>
-          <Col className="mt-4" sm={12}>
-            <Card><Card.Title>Posts
-              </Card.Title>
+				      <Card>
+				        <Card.Title className="px-4 pt-4 pb-0">Filter by Post category</Card.Title>
+				        <Card.Body>
+                    <form onSubmit={this.handleFormSubmit}>
+					            {theCategoryOptions}
+                      <div className="form-group">
+				                <div>Show posts with selected category</div>
+                        <button className="btn btn-primary mt-2" type="submit">
+                          Clear Selection
+                        </button>
+                      </div>
+                    </form>
+				        </Card.Body>
+				      </Card>
+            </Col>
+				  </Row>
+			  </Container>
+        <Container className="Posts">
+          <Row>
+            <Col sm={12}>
+              <Card>
+							  <Card.Title>Posts</Card.Title>
                 <ListGroup>
                   {thePosts}
                 </ListGroup>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
 		  </>
     )
   }
