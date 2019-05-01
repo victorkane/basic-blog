c4ad780e02dbaf67465faf8eb8922a5abf637c45
Victor Kane <victorkane@gmail.com>
2019-05-01 19:06:15 -0400

    Fix FullPost errors, making adjustments for context data being fetched from an external API, and change the browser title with useEffect making use of the Browser API to add the individual post title

--- src/pages/Post/FullPost.js
+++ src/pages/Post/FullPost.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { PostListContext } from "../../context"
 import moment from 'moment'
 import Container from 'react-bootstrap/Container'
@@ -9,25 +9,42 @@ import './index.scss'
 
 const FullPost = (props) => {
 	const context = useContext(PostListContext)
-  const postList = context.somePosts
-  const fullPost = postList.find(element => {
-    return (parseInt(element.id) === parseInt(props.id))
-  })
-  // console.log(fullPost)
-  return (
-    <Container className="Post">
-      <Row>
-        <Col>
-          <Card className="mt-2 px-4 py-4">
-            <Card.Title>{fullPost.title}</Card.Title>
-            <Card.Text>{fullPost.body}</Card.Text>
-            <Card.Text><small><strong>{fullPost.category}</strong></small></Card.Text>
-            <Card.Text><small>{moment(fullPost.postDate).format('MM/DD/YYYY')}</small></Card.Text>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  )
+  const [postList, setPostList] = useState([])
+  const [fullPost, setFullPost] = useState({})
+  useEffect( () => {
+    if (fullPost.title) {
+      const docTitle = document.title
+      document.title = `${docTitle} | ${fullPost.title}`
+    }
+  }, [fullPost])
+  if (postList.length > 0) {
+    // console.log('the full post being rendered',  fullPost)
+    return (
+      <Container className="Post">
+        <Row>
+          <Col>
+            <Card className="mt-2 px-4 py-4">
+              <Card.Title>{fullPost.title}</Card.Title>
+              <Card.Text>{fullPost.body}</Card.Text>
+              <Card.Text><small><strong>{fullPost.category}</strong></small></Card.Text>
+              <Card.Text><small>{moment(fullPost.postDate).format('MM/DD/YYYY')}</small></Card.Text>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  } else {
+    // first time only, once context is here
+    if (Object.keys(context.somePosts).length > 0 && postList.length === 0) {
+      // console.log('context in full post', context)
+      const theFullPost = context.somePosts.find(element => {
+        return (parseInt(element.id) === parseInt(props.id))
+      })
+      setPostList(context.somePosts)
+      setFullPost(theFullPost)
+    }
+    return <div className="spinner-border text-primary m-4" role="status"> <span className="sr-only">Loading...</span> </div>
+  }
 }
 
 export default FullPost
