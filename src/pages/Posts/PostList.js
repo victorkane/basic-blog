960366b3700dabf927c872188243272522358c93
Victor Kane <victorkane@gmail.com>
2019-06-08 19:06:43 -0400

    cleanup code

--- src/pages/Posts/PostList.js
+++ src/pages/Posts/PostList.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import useLocalStorage from './../../hooks/useLocalStorage'
+import useLocalStorage from '../../hooks/useLocalStorage'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
